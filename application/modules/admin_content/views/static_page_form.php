340c068d9a239df70d954adafee514418316ff30
Ilham Mudzakir <ilhamudzakir@gmail.com>
2017-04-17 01:50:40 +0700

    backend

--- application/modules/admin_content/views/static_page_form.php
+++ application/modules/admin_content/views/static_page_form.php
@@ -38,6 +38,6 @@
               </div>
             </div>
           </div>
-<!-- include libraries(jQuery, bootstrap) -->
+
 <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.3/summernote.css" rel="stylesheet">
 <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.3/summernote.js"></script>
