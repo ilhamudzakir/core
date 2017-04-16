1b71e657c3083c3c2e9372d8f9070e3567ee7953
Ilham Mudzakir <ilhamudzakir@gmail.com>
2017-04-16 19:10:02 +0700

    modules
    fd

--- database/core_decode.sql
+++ database/core_decode.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2017 at 03:54 PM
+-- Generation Time: Apr 16, 2017 at 02:08 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -30,9 +30,9 @@ CREATE TABLE `dc_default` (
   `id` int(100) NOT NULL,
   `name_group` varchar(250) NOT NULL,
   `date_created` datetime NOT NULL,
-  `date_modified` datetime NOT NULL,
+  `date_modified` datetime DEFAULT NULL,
   `id_creator` int(250) NOT NULL,
-  `id_modifier` int(250) NOT NULL
+  `id_modifier` int(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,10 +45,69 @@ CREATE TABLE `dc_groups` (
   `id` int(100) NOT NULL,
   `name_group` varchar(250) NOT NULL,
   `date_created` datetime NOT NULL,
+  `date_modified` datetime DEFAULT NULL,
+  `id_creator` int(250) NOT NULL,
+  `id_modifier` int(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dc_groups`
+--
+
+INSERT INTO `dc_groups` (`id`, `name_group`, `date_created`, `date_modified`, `id_creator`, `id_modifier`) VALUES
+(1, 'Super Admin', '2017-04-13 12:29:39', NULL, 1, NULL),
+(5, 'Admin', '2017-04-13 15:24:27', NULL, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dc_icons`
+--
+
+CREATE TABLE `dc_icons` (
+  `id` int(100) NOT NULL,
+  `name_icons` varchar(250) NOT NULL,
+  `date_created` datetime NOT NULL,
   `date_modified` datetime NOT NULL,
   `id_creator` int(250) NOT NULL,
   `id_modifier` int(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dc_icons`
+--
+
+INSERT INTO `dc_icons` (`id`, `name_icons`, `date_created`, `date_modified`, `id_creator`, `id_modifier`) VALUES
+(1, 'fa fa-file-text', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(2, 'icon-custom-home', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(3, 'icon-custom-thumb', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(4, 'icon-custom-settings', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(5, 'fa fa-adjust', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(6, 'fa fa-anchor', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(7, 'fa fa-archive', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(8, 'fa fa-arrows', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(9, 'fa fa-arrows-h', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(10, 'fa fa-arrows-v', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(11, 'fa fa-asterisk', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(12, 'fa fa-ban', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(13, 'fa fa-bar-chart-o', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(14, 'fa fa-barcode', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(15, 'fa fa-bars', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(16, 'fa fa-beer', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(17, 'fa fa-bell', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(18, 'fa fa-bell-o', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(19, 'fa fa-bolt', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(20, 'fa fa-book', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(21, 'fa fa-bookmark', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(22, 'fa fa-bookmark-o', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(23, 'fa fa-briefcase', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(24, 'fa fa-bug', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(25, 'fa fa-building-o', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(26, 'fa fa-bullhorn', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(27, 'fa fa-bullseye', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(28, 'fa fa-calendar', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(29, 'fa fa-calendar-o', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0),
+(30, 'fa fa-camera', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -64,9 +123,9 @@ CREATE TABLE `dc_menu` (
   `icon` varchar(100) NOT NULL,
   `position` int(100) NOT NULL,
   `date_created` datetime NOT NULL,
-  `date_modified` datetime NOT NULL,
+  `date_modified` datetime DEFAULT NULL,
   `id_creator` int(250) NOT NULL,
-  `id_modifier` int(250) NOT NULL
+  `id_modifier` int(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -74,8 +133,13 @@ CREATE TABLE `dc_menu` (
 --
 
 INSERT INTO `dc_menu` (`id`, `name_menu`, `sub_menu`, `target`, `icon`, `position`, `date_created`, `date_modified`, `id_creator`, `id_modifier`) VALUES
-(1, 'Content', 'none', 'admin_content', 'icon-custom-thumb', 1, '2017-04-06 13:29:28', '2017-04-06 07:23:35', 1, 0),
-(2, 'Static Page', '1', 'static_page', '', 1, '2017-04-06 10:24:25', '2017-04-06 12:29:26', 1, 1);
+(1, 'Content', '0', 'admin_content', 'icon-custom-thumb', 1, '0000-00-00 00:00:00', '2017-04-13 11:13:46', 0, 1),
+(2, 'Static Page', '1', 'static_page', '', 1, '2017-04-06 10:24:25', '2017-04-06 12:29:26', 1, 1),
+(3, 'Settings System', '0', 'setting_system', 'icon-custom-settings', 2, '2017-04-13 00:00:00', '0000-00-00 00:00:00', 1, 0),
+(4, 'Menu', '3', 'menu', '', 1, '2017-04-13 00:00:00', '2017-04-13 00:00:00', 1, 0),
+(6, 'User Accsess', '3', 'user_accsess', 'fa fa-bars', 3, '0000-00-00 00:00:00', '2017-04-13 11:15:14', 0, 1),
+(7, 'User Groups', '3', 'user_groups', 'none', 4, '2017-04-13 11:15:03', NULL, 1, NULL),
+(8, 'User', '3', 'user', 'none', 2, '2017-04-16 18:04:14', NULL, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -86,8 +150,34 @@ INSERT INTO `dc_menu` (`id`, `name_menu`, `sub_menu`, `target`, `icon`, `positio
 CREATE TABLE `dc_menu_accsess` (
   `id` int(100) NOT NULL,
   `id_menu` int(100) NOT NULL,
-  `id_group` int(100) NOT NULL
+  `id_group` int(100) NOT NULL,
+  `accsess` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dc_menu_accsess`
+--
+
+INSERT INTO `dc_menu_accsess` (`id`, `id_menu`, `id_group`, `accsess`) VALUES
+(12, 1, 0, 0),
+(13, 2, 0, 1),
+(14, 3, 0, 0),
+(15, 4, 0, 0),
+(16, 6, 0, 0),
+(17, 7, 0, 0),
+(18, 1, 1, 1),
+(19, 2, 1, 1),
+(20, 3, 1, 1),
+(21, 4, 1, 1),
+(22, 6, 1, 1),
+(23, 7, 1, 1),
+(24, 1, 5, 1),
+(25, 2, 5, 1),
+(26, 3, 5, 0),
+(27, 4, 5, 0),
+(28, 6, 5, 0),
+(29, 7, 5, 0),
+(30, 8, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -110,9 +200,9 @@ CREATE TABLE `dc_static_content` (
 --
 
 INSERT INTO `dc_static_content` (`id`, `title`, `content`, `date_created`, `date_modified`, `id_creator`, `id_modifier`) VALUES
-(12, 'adasd', 'hasjkhajksdsdadas', '0000-00-00 00:00:00', '2017-04-05 16:23:04', 0, 1),
-(13, 'sdf', 'sdfdsfsd', '2017-04-05 16:33:32', NULL, 1, NULL),
-(15, 'd adasd as', 'dadas asd asd as', '2017-04-05 16:33:45', NULL, 1, NULL);
+(15, 'd adasd as', 'dadas asd asd as', '2017-04-05 16:33:45', NULL, 1, NULL),
+(16, 'daf', 'fsfa', '2017-04-07 00:40:28', NULL, 1, NULL),
+(17, 'fsafa', 'fsafas', '2017-04-16 16:54:02', NULL, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -140,19 +230,8 @@ CREATE TABLE `dc_user` (
 --
 
 INSERT INTO `dc_user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `photo`, `user_group`, `date_created`, `date_modified`, `id_creator`, `id_modifier`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 'admin', 'webmaster', 'dasdasdas', 1, '2017-04-05 04:29:53', '0000-00-00 00:00:00', 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dc_user_groups`
---
-
-CREATE TABLE `dc_user_groups` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
-  `group_id` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(1, 'admin', '89ccfac87d8d06db06bf3211cb2d69ed', 'admin@gmail.com', 'admin', 'webei', '16807164_1234243823400398_1687302977082263434_n.jpg', 1, '0000-00-00 00:00:00', '2017-04-16 16:31:15', 0, 1),
+(2, 'admin4', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com', 'admin', 'admin', '', 5, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -168,6 +247,12 @@ ALTER TABLE `dc_default`
 -- Indexes for table `dc_groups`
 --
 ALTER TABLE `dc_groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dc_icons`
+--
+ALTER TABLE `dc_icons`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -207,27 +292,32 @@ ALTER TABLE `dc_default`
 -- AUTO_INCREMENT for table `dc_groups`
 --
 ALTER TABLE `dc_groups`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `dc_icons`
+--
+ALTER TABLE `dc_icons`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `dc_menu`
 --
 ALTER TABLE `dc_menu`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `dc_menu_accsess`
 --
 ALTER TABLE `dc_menu_accsess`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `dc_static_content`
 --
 ALTER TABLE `dc_static_content`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `dc_user`
 --
 ALTER TABLE `dc_user`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
